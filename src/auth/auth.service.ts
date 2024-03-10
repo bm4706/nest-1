4f8beb9d5254090757d4bf82bfbb978ab8eb80ab
bm4706 <bm4706@naver.com>
2024-03-10 20:42:32 +0900

    비밀번호 암호화 기능 구현

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpException, HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserDTO } from './dto/user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -20,14 +21,18 @@ export class AuthService {
     }
 
     // 로그인
-    async validateUser(UserDTO: UserDTO) : Promise<UserDTO | undefined> {
+    async validateUser(UserDTO: UserDTO) : Promise<string | undefined> {
         let userFind: UserDTO = await this.userService.findByFields({
             where: {username: UserDTO.username}
         });
+        // 암호화 확인
+        const validatePassword = await bcrypt.compare(UserDTO.password, userFind.password) /*입력한 비밀번호와 db에 저장된 비밀번호 비교*/
         // 사용자를 찾지 못했을 경우 + 비밀번호가 틀렸을 경우
-        if(!userFind || UserDTO.password !== userFind.password) {
-            throw new UnauthorizedException();
+        if(!userFind || !validatePassword) {
+            throw new UnauthorizedException('로그인 실패!');
         }
-        return userFind;
+        return "로그인 성공!";
     }
+
+    
 }
